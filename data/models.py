9a3d941f6aa0d1295b0533ab70a43c8973ddfa85
Kyle Raney <raney24fcb@gmail.com>
2018-11-01 00:35:58 -0400

    db config

--- data/models.py
+++ data/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+# change true/false to yes/no for forms
+BOOL_CHOICES = ((True, 'Yes'), (False, 'No'))
+
 class Company(models.Model):
     name = models.CharField(max_length=200)
     date_added = models.DateTimeField('date added', auto_now_add=True)
     stage = models.ForeignKey('Stage', on_delete=models.CASCADE, blank=True, null=True)
     website = models.URLField(blank=True)
     email_address = models.EmailField(blank=True)
-    investor = models.ForeignKey('Investor', on_delete=models.CASCADE, blank=True, null=True)
+    
     
 
     class Meta:
@@ -15,16 +18,26 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
-class CompanyStage(models.Model):
+class CompanyReport(models.Model):
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
     funding = models.IntegerField('Funding', default=0, blank=True, null=True)
-    product_stage = models.ForeignKey('ProductStage', blank=True, null=True)
+    product_stage = models.ForeignKey('ProductStage', on_delete=models.CASCADE, blank=True, null=True)
+    has_customers = models.BooleanField(choices=BOOL_CHOICES, null=True)
+    revenue = models.IntegerField('Revenue', default=0, blank=True, null=True)
+    fulltime_employees = models.IntegerField('Fulltime Employees', default=0, blank=True, null=True)
+    investor = models.ForeignKey('Investor', on_delete=models.CASCADE, blank=True, null=True)
 
 class Stage(models.Model):
+    
+
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     funding = models.IntegerField('Funding', default=0, blank=True, null=True)
-    product_stage = models.ForeignKey('ProductStage', blank=True, null=True)
-    has_customers = 
+    product_stage = models.ForeignKey('ProductStage', on_delete=models.CASCADE, blank=True, null=True)
+    has_customers = models.BooleanField(choices=BOOL_CHOICES, null=True)
+    revenue = models.IntegerField('Revenue', default=0, blank=True, null=True)
+    fulltime_employees = models.IntegerField('Fulltime Employees', default=0, blank=True, null=True)
+
 
     def __str__(self):
         if len(self.title.split()) > 1:
--- startup/urls.py
+++ startup/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('data/', include('data.urls')),
+    path('data/', include('data.urls'), name='data'),
+    path('', RedirectView.as_view(url='/data', permanent=False), name='index'),
 ]
